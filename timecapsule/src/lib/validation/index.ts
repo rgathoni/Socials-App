c931d222623bbdcf6cf0a02a38201830c4df876c
Rose <gathoni@uni.minerva.edu>
2024-06-25 20:37:22 -0400

    Other Components

--- timecapsule/src/lib/validation/index.ts
+++ timecapsule/src/lib/validation/index.ts
@@ -7,9 +7,18 @@ export const SignupValidation = z.object({
   password: z.string().min(8, { message: "Password must be at least 8 characters." }),
 });
 
+
 export const SigninValidation = z.object({
   email: z.string().email(),
   password: z.string().min(8, { message: "Password must be at least 8 characters." }),
+});
+
+export const ProfileValidation = z.object({
+  file: z.custom<File[]>(),
+  name: z.string().min(2, { message: "Name must be at least 2 characters." }),
+  username: z.string().min(2, { message: "Name must be at least 2 characters." }),
+  email: z.string().email(),
+  bio: z.string(),
 });
 
 // ============================================================
